e511b5d255ba4824c4acef2d896018b0cd114bd8
Hritik Kumar <82868175+hritikkumarpd@users.noreply.github.com>
2025-09-11 09:36:17 +0000

    Pattern

---
+++ Step_1_LEARN_THE_BASICS/Lectures-2/rectanglePattern.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class rectanglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
